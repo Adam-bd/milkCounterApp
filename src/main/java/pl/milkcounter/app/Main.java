4ba9dc8cc3ae240a8b31678c45d2853267537866
Adam-bd <adambyf1324.5@gmail.com>
2025-12-27 18:43:31 +0100

    class MilkPortion is ready

--- src/main/java/pl/milkcounter/app/Main.java
+++ src/main/java/pl/milkcounter/app/Main.java
@@ -1,7 +1,18 @@
 package pl.milkcounter.app;
 
+import pl.milkcounter.model.MilkPortion;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String dataTekst1 = "25.12.2025";
+        String dataTekst2 = "26.06.2026";
+        DateTimeFormatter polskiFormat = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        MilkPortion milkPortion = new MilkPortion(LocalDate.parse(dataTekst1, polskiFormat), 150);
+        System.out.println(milkPortion.isExpired(LocalDate.parse(dataTekst2, polskiFormat)));
+        System.out.println(milkPortion.toString());
+
     }
 }
--- src/main/java/pl/milkcounter/model/MilkPortion.java
+++ src/main/java/pl/milkcounter/model/MilkPortion.java
@@ -1,14 +1,33 @@
 package pl.milkcounter.model;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class MilkPortion {
     private LocalDate dateOfFreezing;
     private int portionOfMilk; //in milliliters
+    private DateTimeFormatter polskiFormat = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
-    MilkPortion (LocalDate dateOfFreezing, int portionOfMilk) {
+    public MilkPortion (LocalDate dateOfFreezing, int portionOfMilk) {
         this.dateOfFreezing = dateOfFreezing;
         this.portionOfMilk = portionOfMilk;
     }
 
+    public boolean isExpired(LocalDate dateToCheck) {
+        LocalDate expirationDate = this.dateOfFreezing.plusMonths(6);
+        return dateToCheck.isAfter(expirationDate);
+    }
+
+    public LocalDate getDateOfFreezing() {
+        return dateOfFreezing;
+    }
+
+    public int getPortionOfMilk() {
+        return portionOfMilk;
+    }
+
+    @Override
+    public String toString() {
+        return "Mleko (" + portionOfMilk + "ml) z dnia " + dateOfFreezing;
+    }
 }
