ee894cfad607d0fdbdf39bb11cc8ba1512493b8f
Adam-bd <adambyf1324.5@gmail.com>
2025-12-27 22:55:57 +0100

    class MilkStorage is ready

--- src/main/java/pl/milkcounter/app/Main.java
+++ src/main/java/pl/milkcounter/app/Main.java
@@ -1,6 +1,7 @@
 package pl.milkcounter.app;
 
 import pl.milkcounter.model.MilkPortion;
+import pl.milkcounter.model.MilkStorage;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -10,9 +11,23 @@ public class Main {
         String dataTekst1 = "25.12.2025";
         String dataTekst2 = "26.06.2026";
         DateTimeFormatter polskiFormat = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        MilkPortion milkPortion = new MilkPortion(LocalDate.parse(dataTekst1, polskiFormat), 150);
-        System.out.println(milkPortion.isExpired(LocalDate.parse(dataTekst2, polskiFormat)));
-        System.out.println(milkPortion.toString());
+        MilkPortion milkPortion1 = new MilkPortion(LocalDate.parse(dataTekst1, polskiFormat), 150);
+        MilkPortion milkPortion2 = new MilkPortion(LocalDate.parse("27.12.2025", polskiFormat), 120);
+        MilkPortion milkPortion3 = new MilkPortion(LocalDate.parse("20.10.2025", polskiFormat), 130);
+//        System.out.println(milkPortion1.isExpired(LocalDate.parse(dataTekst2, polskiFormat)));
+//        System.out.println(milkPortion1.toString());
+
+        MilkStorage storage = new MilkStorage();
+        storage.addPortion(milkPortion1);
+        storage.addPortion(milkPortion2);
+        storage.addPortion(milkPortion3);
+        storage.addPortion(new MilkPortion(LocalDate.parse(dataTekst2, polskiFormat), 150));
+
+        System.out.println(storage.getPortions());
+        System.out.println(storage.getTotal());
+
+        System.out.println(storage.takeMilk(LocalDate.parse("13.05.2026", polskiFormat), 240));
+        System.out.println(storage.getTotal());
 
     }
 }
---
+++ src/main/java/pl/milkcounter/model/MilkStorage.java
@@ -0,0 +1,55 @@
+package pl.milkcounter.model;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class MilkStorage {
+    private List<MilkPortion> portions;
+
+    public MilkStorage() {
+        this.portions = new ArrayList<>();
+    }
+
+    public void addPortion(MilkPortion portion) {
+        portions.add(portion);
+        portions.sort(Comparator.comparing(MilkPortion::getDateOfFreezing));
+    }
+
+    public int getTotal() {
+        int totalMilk = 0;
+        for(MilkPortion portion : portions){
+            totalMilk += portion.getPortionOfMilk();
+        }
+        return totalMilk;
+    }
+
+    public List<MilkPortion> getPortions() {
+        return portions;
+    }
+
+    public List<MilkPortion> takeMilk(LocalDate todayDate, int amountOfMilkNeeded) {
+        List<MilkPortion> portionsToTakeOut = new ArrayList<>(); //take out of freezer
+        List<MilkPortion> portionsToThrowOut = new ArrayList<>();
+        int amountOfMilkCollected = 0;
+
+        for(MilkPortion portion : portions) {
+            if (portion.isExpired(todayDate)) {
+                System.out.println("Uwaga!!! Znaleziono mleko po terminie. Wyrzuć mleko z dnia: " + portion.getDateOfFreezing());
+                portionsToThrowOut.add(portion);
+                continue;
+            }
+
+            portionsToTakeOut.add(portion);
+            amountOfMilkCollected += portion.getPortionOfMilk();
+
+            if (amountOfMilkCollected >= amountOfMilkNeeded) {
+                break;
+            }
+        }
+        portions.removeAll(portionsToTakeOut);
+        portions.removeAll(portionsToThrowOut);
+        return portionsToTakeOut;
+    }
+}
